d813bca57a56c381cf6211fe6892e6a61b59f096
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-07-12 10:25:38 -0400

    Set up database table and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+### Seeds for burger database
+ 
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Bacon Double Cheeseburger', false, '2016-07-03 15:33:06');
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('4x4 Burger', false, '2016-07-04 12:53:12');
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Luther Burger', false, '2016-07-06 10:42:41');
